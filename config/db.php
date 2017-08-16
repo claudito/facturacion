6001bdb93a57e9380605fb886a9be981e69b2962
Luis Augusto Claudio Ponce <megabyte1507@gmail.com>
2017-08-15 22:18:49 -0500

    first commit

---
+++ config/db.php
@@ -0,0 +1,25 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'root');//Usuario de tu base de datos
+define('DB_PASS', 'userperutecdb');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'facturacion');//Nombre de la base de datos
+ 
+/*Datos de la empresa*/
+define('NOMBRE_EMPRESA', 'PerútTec Enterprises SAC');
+define('DIRECCION_EMPRESA', 'Jr. Rio Putumayo, Mz S Lote 19 Urb.
+Canto Rey - San Juan de Lurigancho');
+define('TELEFONO_EMPRESA', '997935085');
+define('EMAIL_EMPRESA', 'soluciones@perutec.com.pe');
+define('TAX', '18');
+
+/*-------------------------------------------------
+Los datos por defecto del usuario administrador son:
+#usuario: admin
+#contraseña: admin
+Los datos de inicio de sesion podran ser modificados 
+desde el panel de control del sistema
+---------------------------------------------------*/
+
+
+?>
